316ac06a40c46ecbdba20823565d6a590414a225
sachin_sde <ssachin.s@myclnq.co.in>
2025-06-26 21:00:53 +0530

    resure

--- src/components/HandleState.tsx
+++ src/components/HandleState.tsx
@@ -3,11 +3,8 @@ interface ISTATE{
     name:string;
     title:string
 }
-
-
 export let HandleState: React.FC =()=>{
     const [state, setState] = useState<ISTATE>({name:"Saxhin",title:"Developer"});
-
     return (
       <>
       <h1>Here we handle state in TypeScript</h1>
